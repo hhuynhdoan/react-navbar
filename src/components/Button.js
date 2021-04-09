e1a107bed899d2d45796871f53e97dd22da6bb79
huynhdoan <dntrh0425@gmail.com>
2021-04-09 13:28:11 +0700

    navbar

---
+++ src/components/Button.js
@@ -0,0 +1,32 @@
+import Reac from 'react';
+import './Button.css'
+
+const STYLES =[
+    'btn--primary',
+    'btn--outline'
+]
+
+const SIZES = [
+    'btn--medium',
+    'btn--large'
+]
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    butttonSize
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+
+    const checkButtonSize = SIZES.includes(butttonSize) ? butttonSize : SIZES[0]
+
+    return (
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+        {children}
+        </button>
+   
+    )
+        
+}
